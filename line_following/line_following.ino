ceb46449402b079660e2f8f21d6a81cefe9a8c3f
SasikaA073 <sasikapamith2016@gmail.com>
2023-12-08 08:06:27 +0530

    refactor code

--- line_following/line_following.ino
+++ line_following/line_following.ino
@@ -1,252 +1,241 @@
-
-
-// ir array pins and globals
-uint8_t const IR_pins[]={A8,A9,A10,A11,A12,A13,A14,A15};
+// Define pin numbers
+uint8_t const IR_pins[] = {A8, A9, A10, A11, A12, A13, A14, A15};
 uint16_t sensorValues[8];
-uint16_t const threshold=250 ;
+uint16_t const threshold = 250;
 uint8_t IR_digital_array[8];
 
-//motor driver pins- ENTC
-uint8_t const INA_1=2;
-uint8_t const INB_1=23;
-uint8_t const INA_2=3;
-uint8_t const INB_2=22;
+// ENTC - Motor driver pins
+uint8_t const INA_1 = 2;
+uint8_t const INB_1 = 23;
+uint8_t const INA_2 = 3;
+uint8_t const INB_2 = 22;
 
-//PID & errors
-float const kp = 0.0067;//5-54-60-65-70
-float  const kd = 0.053;//0.018// 28//35// 
-float const ki = 0.0005;
-uint16_t const Basespeed=60; 
+// PID & errors
+float const line_kp = 0.0067; //5-54-60-65-70
+float const line_kd = 0.053; //0.018// 28//35// 
+float const line_ki = 0.0005;
+uint16_t const baseSpeed = 60;
 
-int previouserror=0;
-int error;//temporary use for checking the ir array
-int control_signal_1;//for temp use of tunning and debuging- can be made local in pid func
-int control_signal_2;//""
-int count=0;//""
-int total_error=0;//""
-
-int integral = 0;
-
-//line follower logic
+int previousLineError = 0;
+int line_error;  //temporary use for checking the ir array           
+int control_signal_1;  //for temp use of tunning and debuging- can be made local in pid func   
+int control_signal_2;     
+int IR_trigger_count = 0; 
+int total_line_error = 0; 
+int line_integral = 0;
 
 uint8_t checkpoint = 0;
 
 void setup() {
-  
-  // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+    // Initialize Builtin led
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  //Motor Driver Pins
-  pinMode(INA_1, OUTPUT);
-  pinMode(INB_1, OUTPUT);
-  pinMode(INA_2, OUTPUT);
-  pinMode(INB_2, OUTPUT);
+    // Initialize Serial Monitor
+    Serial.begin(9600);
 
-  // Configure IR array pins
-  for (uint8_t i = 0;i<8;i++){pinMode(IR_pins[i],INPUT);}
-  
-  //start indicator
-  for (uint8_t i = 0; i<10;i++){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(200);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(200);
-    }
+    // Initialize motor driver
+    initializeMotorDriver();
+
+    // Initialize IR array 
+    initializeIRArray();
+
+    // Initialize indicator
+    startIndicator();
 }
-
 
 void loop() {
-
-
-pid_line_following();
-delay(30);
-junction_find();
-
+    pid_line_following();
+    delay(30);
+    find_junction();
 }
 
-
-/////////////////************************************///////////////////
-
-void Rotate_Wheels(int speedA, int speedB) {
-
-//shock();
-
-//speedA = constrain(speedA,-150,150);
-  
-  
-  if (speedA > 0) {
-  
-  digitalWrite(INB_1, LOW);
-  analogWrite(INA_1, speedA); 
-  }else{
-
-  digitalWrite(INB_1, HIGH);
-  analogWrite(INA_1, -speedA);   
-  }
-  if (speedB > 0) {
-  digitalWrite(INB_2, LOW);
-  analogWrite(INA_2, speedB);
-   
-  }else{
-  digitalWrite(INB_2, HIGH);
-  analogWrite(INA_2, -speedB);  
-    
-  }
-}
-
-void shock(){
-  Rotate_Wheels(100,100);
-  delay(40);
-  Rotate_Wheels(0,0);
-  }
-  
-uint8_t digital_read(uint8_t pin){
-  int IR_analog_value=analogRead(pin);
-  if (IR_analog_value<threshold){return 1;}//the only place to change when switch from black line to white?
-  else{return 0;}
-}
-
-//int error_v(){ 
-//    
-//  int error_v; //return value
-//  for (uint8_t i=0;i<8;i++){
-//    IR_digital_array[i]=digital_read(IR_pins[i]); // see globals 
-//    sensorValues[i]=analogRead(IR_pins[i]); // see globals
-//  }
-//  //error_v  = IR_digital_array[0]*800+IR_digital_array[1]*500+IR_digital_array[2]*200+IR_digital_array[5]*-200+IR_digital_array[6]*-500+IR_digital_array[7]*-800;
-//  error_v  = 
-//  return error_v;
-//}
-
-int error_v(){ 
-  total_error=0;
-  count =0 ;   
-  int error_v; //return value
-  for (uint8_t i=0;i<8;i++){
-    IR_digital_array[i]=digital_read(IR_pins[i]); // see globals 
-    if (IR_digital_array[i] == 1){
-      count+=1;
-      total_error =total_error+ 1000*i;
-      sensorValues[i]=analogRead(IR_pins[i]); // see globals
-      }
-      
-  sensorValues[i]=analogRead(IR_pins[i]);
-  }
-  
-  if (count!=0){
-    error_v= total_error/count-3500;
-  }else{
-    
-    
+// //////////////////////////////////////////////////////
+// function to get debug output of IR array
+void PIDLineDebug() {
+    for (uint8_t i = 0; i < 8; i++) {
+        Serial.print(sensorValues[i]);
+        Serial.print('\t');
     }
-  return error_v;
-}
-
-void pid_line_following()
-{
-  
-  int currenterror=error_v();
-  error = currenterror;// global
-  integral += currenterror;// global
-  int delta=currenterror-previouserror;
-  float CT = currenterror*kp + delta*kd + integral*ki;
-  previouserror=currenterror;//global previouserror
-  control_signal_2=  Basespeed+round(constrain(CT,-255,255));//basespeed global
-  control_signal_1=  Basespeed-round(constrain(CT,-255,255));
- // motor_control(1,control_signal_1);motor_control(2,control_signal_2);
-  Rotate_Wheels(control_signal_1,control_signal_2);
-  
-  delay(4);
-
-  PID_debug();//comment out in normal operation
-}
-
-
-void junction_find(){
-  //if 6 or more ir sensors detected, identify if it is 90 bend or T junct, wait..
-  //..(either a certain distance or a time) and  
-  uint16_t turn_speed = 65;
-  uint16_t turn_time  = 130;
-  uint8_t no_iterations = 8;
-  
-  if (count>=5){
-    bool left_turn = IR_digital_array[0]==1;//store the current line data
-    bool right_turn = IR_digital_array[7]==1;
-    
-    //keep running pid line follow for around half a second or let go straight 
-    for (uint8_t i = 0;i<no_iterations;i++){ // (14)previous value
-      pid_line_following();
-      delay(34); // try changint the value here 
+    Serial.println('\t');
+    for (uint8_t i = 0; i < 8; i++) {
+        Serial.print(IR_digital_array[i]);
+        Serial.print('\t');
     }
+    Serial.print(control_signal_1);
+    Serial.print('\t');
+    Serial.print(control_signal_2);
+    Serial.print('\t');
+    Serial.print(line_error);
+    Serial.print('\t');
+    Serial.print(IR_trigger_count);
+    Serial.print('\t');
+    Serial.println(total_line_error);
+}
+
+// function to move the robot along the line according to PID
+void pid_line_following() {
+
+    int currentLineError = get_line_following_error();
     
-    //delay(500);//wait
-    //pid_line_following();//update line data
-    
-    // line data has changed and "pid_line_following" has updated the values
-    if (left_turn && right_turn){
-      if (count == 0){
-      // T junction encountered
-        // turn left/ right with priority 
-        Rotate_Wheels(0,turn_speed);
-        delay (turn_time);   
+    line_error = currentLineError;
+    line_integral += currentLineError;
+
+    int line_delta = currentLineError - previousLineError;
+    float line_CT = currentLineError * line_kp + line_delta * line_kd + line_integral * line_ki;
+    previousLineError = currentLineError;
+
+    control_signal_2 = baseSpeed + round(constrain(line_CT, -255, 255));
+    control_signal_1 = baseSpeed - round(constrain(line_CT, -255, 255));
+
+    rotate_wheels(control_signal_1, control_signal_2);
+
+    delay(4);
+
+    PIDLineDebug(); // comment out in normal operation
+}
+
+
+
+// function to initialize IR array
+void initializeIRArray() {
+    for (uint8_t i = 0; i < 8; i++) {
+        pinMode(IR_pins[i], INPUT);
+    }
+}
+
+// function to initialize motor driver
+void initializeMotorDriver() {
+    pinMode(INA_1, OUTPUT);
+    pinMode(INB_1, OUTPUT);
+    pinMode(INA_2, OUTPUT);
+    pinMode(INB_2, OUTPUT);
+}
+
+// function to start led indicator
+void startIndicator() {
+    for (uint8_t i = 0; i < 10; i++) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        delay(200);
+        digitalWrite(LED_BUILTIN, LOW);
+        delay(200);
+    }
+}
+
+// function to rotate wheels
+void rotate_wheels(int speedA, int speedB) {
+    if (speedA > 0) {
+        digitalWrite(INB_1, LOW);
+        analogWrite(INA_1, speedA);
+    } else {
+        digitalWrite(INB_1, HIGH);
+        analogWrite(INA_1, -speedA);
+    }
+    if (speedB > 0) {
+        digitalWrite(INB_2, LOW);
+        analogWrite(INA_2, speedB);
+    } else {
+        digitalWrite(INB_2, HIGH);
+        analogWrite(INA_2, -speedB);
+    }
+}
+
+// function to blink the LED builtin
+void blink_builtin_led() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(100);
+}
+
+// function to get the digital out of the IR array
+uint8_t get_ir_digital_out(uint8_t pin) {
+    int IR_analog_value = analogRead(pin);
+    if (IR_analog_value < threshold) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// function to get the line error
+int get_line_following_error() {
+    total_line_error = 0;
+    IR_trigger_count = 0;
+    int error_v;
+    for (uint8_t i = 0; i < 8; i++) {
+        IR_digital_array[i] = get_ir_digital_out(IR_pins[i]);
+        if (IR_digital_array[i] == 1) {
+            IR_trigger_count += 1;
+            total_line_error = total_line_error + 1000 * i;
+            sensorValues[i] = analogRead(IR_pins[i]);
         }
-      else{
-      // check point
-        checkpoint++;
-        while(true){Rotate_Wheels(0,0);}
-          
-        }  
-      }
+        sensorValues[i] = analogRead(IR_pins[i]);
+    }
+    if (IR_trigger_count != 0) {
+        error_v = total_line_error / IR_trigger_count - 3500;
+    }
+    return error_v;
+}
+
+
+
+// function to kick start the motor
+void shock() {
+    rotate_wheels(100, 100);
+    delay(40);
+    rotate_wheels(0, 0);
+}
+
+
+// function to find junctions, dead ends
+void find_junction() {
+    // TODO: Change value here
+    uint8_t no_iterations = 8;
+
+
+    uint16_t turn_speed = 65;
+    uint16_t turn_time = 130;
     
-    else if (left_turn){
-      
-      if (count == 0){
-        Rotate_Wheels(0,turn_speed);
-        delay (turn_time);
-      }
-      else{
-        // T junc side
-        return ; 
+
+    if (IR_trigger_count >= 5) {
+        bool left_turn = IR_digital_array[0] == 1;
+        bool right_turn = IR_digital_array[7] == 1;
         
-      }
-    }
-    else if (right_turn){
-      
-      if (count == 0){
-        Rotate_Wheels(turn_speed,0);
-        delay (turn_time);
-      }
-      else{return;}
-    }
-    
-  }
-  else if(count == 0){
-    //dead-end //turn back
-    Rotate_Wheels(turn_speed,-turn_speed);
-    delay (turn_time);
+        for (uint8_t i = 0; i < no_iterations; i++) {
+            pid_line_following();
+            delay(34);
+        }
+
+        if (left_turn && right_turn) {
+            if (IR_trigger_count == 0) {
+                rotate_wheels(0, turn_speed);
+                delay(turn_time);
+            } else {
+                checkpoint++;
+                while (true) {
+                    rotate_wheels(0, 0);
+                }
+            }
+        } else if (left_turn) {
+            if (IR_trigger_count == 0) {
+                rotate_wheels(0, turn_speed);
+                delay(turn_time);
+            } else {
+                return;
+            }
+        } else if (right_turn) {
+            if (IR_trigger_count == 0) {
+                rotate_wheels(turn_speed, 0);
+                delay(turn_time);
+            } else {
+                return;
+            }
+        }
+    } else if (IR_trigger_count == 0) {
+        rotate_wheels(turn_speed, -turn_speed);
+        delay(turn_time);
     }
 }
 
 
-void PID_debug(){
-  for (uint8_t i=0;i<8;i++){
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-    }
-  Serial.println('\t');
-  for (uint8_t i=0;i<8;i++){
-    Serial.print(IR_digital_array[i]);
-    Serial.print('\t');
-  }
-  Serial.print(control_signal_1);
-  Serial.print('\t');
-  Serial.print(control_signal_2);
-  Serial.print('\t');
-  Serial.print(error);
-  Serial.print('\t');
-  Serial.print(count);
-  Serial.print('\t');
-  Serial.println(total_error);
-  
-}
+
---
+++ new.py
@@ -0,0 +1,2 @@
+def returnAdd(n1:int=5, n2:int=10):
+    return n1 + n2
